07499d38d1052e7f4fcbeeddfbee313093870e29
Peifeng Liu <pliuphys@gmail.com>
2017-05-26 16:24:31 -0400

    Enabled FMS hit IO

--- StPicoDstMaker/StPicoArrays.h
+++ StPicoDstMaker/StPicoArrays.h
@@ -6,7 +6,7 @@ class StPicoArrays {
   StPicoArrays();
 
   // Should be changed to constexpr once ROOT 6 is available at STAR
-  enum { NAllPicoArrays = 12};
+  enum { NAllPicoArrays = 13};
 
 /// names of the TBranches in the TTree/File
   static const char*   picoArrayNames[NAllPicoArrays];
@@ -18,7 +18,7 @@ class StPicoArrays {
   static int           picoArraySizes[NAllPicoArrays];
 
   enum TypeIndex {Event=0, Track, EmcTrigger, MtdTrigger,
-                  BTowHit, BTofHit, MtdHit, BbcTile, EpdTile,
+                  BTowHit, BTofHit, MtdHit, BbcTile, EpdTile, FmsHit,
                   BEmcPidTraits, BTofPidTraits, MtdPidTraits};
 
 };
